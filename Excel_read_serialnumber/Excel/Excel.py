557bc1515bef246a4f93cd2fdd02e72524834145
Tariq <tariq9853@gmail.com>
2020-03-21 03:01:32 +0100

    sheet formatting and comments.

--- Excel_read_serialnumber/Excel/Excel.py
+++ Excel_read_serialnumber/Excel/Excel.py
@@ -1,8 +1,10 @@
+#----Imports----#
 from openpyxl import load_workbook
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 import argparse
 
+#----Arg parsing----#
 parser = argparse.ArgumentParser(description='Example: Excel.py 19011200020001')
 parser.add_argument("-s","--serial",metavar='',help="Serialnumber from Matas.",required=False,default="")
 group = parser.add_mutually_exclusive_group()
@@ -10,16 +12,23 @@ group.add_argument('-q','--quiet',action='store_true',help='App is quiet')
 group.add_argument('-v','--verbose',action='store_true',help='App is quiet')
 args = parser.parse_args()
 
+#----Arg mapping----#
 matasqr = args.serial
 
+#----Workbook and sheet loading----#
 wb = load_workbook(filename = 'Workbook.xlsx')
 sheet = wb['Sheet1']
+sheet._number_formats = '0.00E+00'
 
+
+#----Functions----#
 def getdatafromcell():
     for i in range(1,3):
         sheet.cell(row=3,column=2).value = matasqr
         print(sheet.cell(row=3,column=i).value)
 
+
+#----Main----#
 if __name__ == "__main__":
     
     if args.quiet:
@@ -35,9 +44,6 @@ if __name__ == "__main__":
     else:
         print("this is else")
 
-    
 
-    #print(sheet_ranges['A1'].value)
-    #print(sheet_ranges['B1'].value)
-
+    #----Save changes to different .xlsx to prevent overriding data----#
     wb.save('WorkbookPy.xlsx')
