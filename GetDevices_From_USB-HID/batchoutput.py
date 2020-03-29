02e3add31d952c4251afad0514f008f7fe24131f
Tariq <tariq9853@gmail.com>
2020-03-29 22:26:40 +0200

    Moved and renamed folders.

--- GetDevices_From_USB-HID/batchoutput.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import random
-
-
-item = subprocess.Popen(["test.bat", str(random.randrange(0,20))] , 
-                         shell=True, stdout=subprocess.PIPE)
-for line in item.stdout:
-    print(line)
--- GetDevices_From_USB-HID/test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set arg1=%1
-echo I wish I had %arg1% eggs!
