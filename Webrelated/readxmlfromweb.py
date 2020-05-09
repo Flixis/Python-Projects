2488e06a4d61c8aea5a1899d5f7497138103db39
Tariq <tariq9853@gmail.com>
2020-05-09 16:27:49 +0200

    Learning to work with json.

--- Webrelated/readxmlfromweb.py
+++ Webrelated/readxmlfromweb.py
@@ -2,5 +2,4 @@ import urllib.request, json
 
 with urllib.request.urlopen("http://welldonelasagna.com/latestrelease.json") as url:
     data = json.loads(url.read().decode())
-    if "Version" in data:
-        print (data["Version"])
+    
