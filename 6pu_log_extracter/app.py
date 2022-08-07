07608d3cd457a3d576d860f632a79db30fd3002d
Tariq Dinmohamed <tariq.dinmohamed@prodrive-technologies.com>
2022-08-07 17:04:04 +0200

    first commit just so I have a backup

---
+++ 6pu_log_extracter/app.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+
+
+def dir_path(string):
+    """
+    Checks if the passed string is a dir.
+    """
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+parser = argparse.ArgumentParser(description='Example: app.py -f <log_to_scan>')
+parser.add_argument("-f","--file",metavar='',help="Log file to scan.",required=True, default=os.getcwd())
+args = parser.parse_args()
+
+logfile = args.file
+
+
+
+with open(logfile) as f:
+    lines = f.read().split('\n')
+    print(lines)
