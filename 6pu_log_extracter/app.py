674432d9127fc035f2d0e5e1abcb7ecac6e088b7
Tariq Dinmohamed <tariq.dinmohamed@prodrive-technologies.com>
2022-08-08 16:07:13 +0200

    Flip filehandler and function_def

--- 6pu_log_extracter/app.py
+++ 6pu_log_extracter/app.py
@@ -18,25 +18,23 @@ parser = argparse.ArgumentParser(description='Example: app.py -f <log_to_scan>')
 parser.add_argument("-f","--file",metavar='',help="Log file to scan.",required=True, default=os.getcwd())
 args = parser.parse_args()
 
-with fileinput.input([args.file]) as logfile:                  
-    def get_test_result_data() -> str:
+
+def get_test_result_data() -> str:
+    with fileinput.input([args.file]) as logfile:                  
         for text in logfile:
             if '- SN: ' in text:
                 text.strip()
                 text = text.replace("- SN: ", "")
                 SN_DUT = text[0:text.find(" - Location")]#read till Location
-                LOCATION_DUT = text[text.find("Location"):text.find("PN")]
+                LOCATION_DUT = text[text.find("Location"):text.find("- PN")]
                 RESULT_DUT = text[text.find("Result"):len(text)]
                 # print(f"{SN_DUT}\n{LOCATION_DUT}\n{RESULT_DUT}")
-                
                 serialized_data = {
-                    'SN' : [SN_DUT],
-                    'LOCATION':[LOCATION_DUT],
-                    'RESULT_DUT':[RESULT_DUT]                   
+                        'SN' : [SN_DUT],
+                        'LOCATION':[LOCATION_DUT],
+                        'RESULT_DUT':[RESULT_DUT]                   
                 }
-                
-                tojson = json.dumps(serialized_data)
-                print(tojson)
+                print(serialized_data)
                 
             elif 'TEST FAILURE Failure reason' in text:
                 text.strip()
@@ -44,8 +42,11 @@ with fileinput.input([args.file]) as logfile:
                 GET_CURRENT_LINE_NUMBER = logfile.filelineno()
                 SET_LINE_NUMBER = linecache.getline(args.file, (GET_CURRENT_LINE_NUMBER+1)).strip()
                 REF_DES_ONLY = SET_LINE_NUMBER[SET_LINE_NUMBER.find("Ref"):len(SET_LINE_NUMBER)].strip()
-                # print(f"{GET_LOCATION} {REF_DES_ONLY}\n")
-                #print(f"{SN_DUT}\n{LOCATION_DUT}\n{RESULT_DUT}\n{REF_DES_ONLY}")
-        if text is None:
-            raise RuntimeError("Could not find keywords")
-    get_test_result_data()
+                
+                serialized_data = {
+                    'LOCATION' : [GET_LOCATION],
+                    'REF_DES':[REF_DES_ONLY],              
+                }
+                serialized_data_json = json.dumps(serialized_data)
+                print(serialized_data_json)
+get_test_result_data()
