cbb6754df89904609ca5dc5c66e27aa6ef7f0e5b
Tariq Dinmohamed <tariq.dinmohamed@prodrive-technologies.com>
2022-08-08 15:49:03 +0200

    Changed stuff

--- 6pu_log_extracter/app.py
+++ 6pu_log_extracter/app.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import fileinput
 import linecache
+import json
 
 
 def dir_path(string):
@@ -17,32 +18,24 @@ parser = argparse.ArgumentParser(description='Example: app.py -f <log_to_scan>')
 parser.add_argument("-f","--file",metavar='',help="Log file to scan.",required=True, default=os.getcwd())
 args = parser.parse_args()
 
-logfile = fileinput.input([args.file])
-
-def get_shannon_memtest_fail() -> tuple:
-    
-    shannon_memtest_refdes_array = []
-    for text in logfile:
-        if 'Shannon memtest failed' in text:
-            shannon_memtest_refdes_array = text
-            print(f"{shannon_memtest_refdes_array.strip()}") #strip \n
-                
-def get_test_result_data() -> str:
-    for text in logfile:
-        if '- SN: ' in text:
-            text.strip()
-            text = text.replace("- ", "")
-            SN_DUT = text[0:text.find("Location")]#read till Location
-            LOCATION_DUT = text[text.find("Location"):text.find("PN")]
-            RESULT_DUT = text[text.find("Result"):len(text)]
-            #print(f"{SN_DUT}\n{LOCATION_DUT}\n{RESULT_DUT}")
-        if 'TEST FAILURE Failure reason' in text:
-            text.strip()
-            GET_LOCATION = text[text.find("L"):text.find("S")].strip()
-            GET_CURRENT_LINE_NUMBER = logfile.filelineno()
-            SET_LINE_NUMBER = linecache.getline(args.file, (GET_CURRENT_LINE_NUMBER+1)).strip()
-            REF_DES_ONLY = SET_LINE_NUMBER[SET_LINE_NUMBER.find("Ref"):len(SET_LINE_NUMBER)].strip()
-            print(f"{GET_LOCATION} {REF_DES_ONLY}")
-            
-get_test_result_data()
-fileinput.close()
+with fileinput.input([args.file]) as logfile:                  
+    def get_test_result_data() -> str:
+        for text in logfile:
+            if '- SN: ' in text:
+                text.strip()
+                text = text.replace("- ", "")
+                SN_DUT = text[0:text.find("Location")]#read till Location
+                LOCATION_DUT = text[text.find("Location"):text.find("PN")]
+                RESULT_DUT = text[text.find("Result"):len(text)]
+                print(f"{SN_DUT}\n{LOCATION_DUT}\n{RESULT_DUT}")
+            elif 'TEST FAILURE Failure reason' in text:
+                text.strip()
+                GET_LOCATION = text[text.find("L"):text.find("S")].strip()
+                GET_CURRENT_LINE_NUMBER = logfile.filelineno()
+                SET_LINE_NUMBER = linecache.getline(args.file, (GET_CURRENT_LINE_NUMBER+1)).strip()
+                REF_DES_ONLY = SET_LINE_NUMBER[SET_LINE_NUMBER.find("Ref"):len(SET_LINE_NUMBER)].strip()
+                print(f"{GET_LOCATION} {REF_DES_ONLY}")
+                print(f"{SN_DUT}\n{LOCATION_DUT}\n{RESULT_DUT}\n{REF_DES_ONLY}")
+        if text is None:
+            raise RuntimeError("Could not find keywords")
+    get_test_result_data()
