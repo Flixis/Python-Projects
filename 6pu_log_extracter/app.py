690cdbb259032a1db4d69d8974d482f5cd568510
Tariq <tariq9853@gmail.com>
2022-08-09 23:50:52 +0200

    getting close too finish line

--- 6pu_log_extracter/app.py
+++ 6pu_log_extracter/app.py
@@ -29,21 +29,32 @@ with fileinput.input([args.file]) as logfile:
             GET_LOCATION = text[text.find("L"):text.find("S")].strip()
             GET_CURRENT_LINE_NUMBER = logfile.filelineno()
             SET_LINE_NUMBER = linecache.getline(args.file, (GET_CURRENT_LINE_NUMBER+1)).strip()
-            REF_DES_ONLY = SET_LINE_NUMBER[SET_LINE_NUMBER.find("Ref"):len(SET_LINE_NUMBER)].replace("RefDes: ","").strip()
+            REF_DES = SET_LINE_NUMBER[SET_LINE_NUMBER.find("Ref"):len(SET_LINE_NUMBER)].replace("RefDes: ","").strip()
         if '- SN: ' in text:
             SN_DUT = text[text.find("- SN: "):text.find(" - Location")].replace("- SN: ","")#read till Location
             LOCATION_DUT = text[text.find("Location:"):text.find("- PN")].replace("Location: ","")
             RESULT_DUT = text[text.find("Result"):len(text)].replace("Result: ","")
             
-            serialized_data_refdes = {
+            serialized_data_results = {
                 'SN' : [SN_DUT.strip()],
-                'LOCATION_FROM_RESULTS':[LOCATION_DUT.strip()],
-                'LOCATION_FROM_REFDES' : [GET_LOCATION.strip()],
-                'RESULT_DUT':[RESULT_DUT.strip()],     
-                'REF_DES':[REF_DES_ONLY.strip()],              
+                'LOCATION':[LOCATION_DUT.strip()],
+                'RESULT_DUT':[RESULT_DUT.strip()],                
             }
-            
-            if serialized_data_refdes['SN'] == "":
+            serialized_data_refdes = {
+                'LOCATION' : [GET_LOCATION.strip()],
+                'REF_DES':[REF_DES.strip()],              
+            } 
+            #Only print if we have data in SN.
+            if serialized_data_results['SN'] == "":
                 pass
             else:
-                print(serialized_data_refdes)
+                if serialized_data_results["LOCATION"] == serialized_data_results["LOCATION"]:
+                    serialized_combined_data = {
+                        'SN' : [SN_DUT.strip()],
+                        'LOCATION':[LOCATION_DUT.strip()],
+                        'RESULT_DUT':[RESULT_DUT.strip()],     
+                        'REF_DES':[REF_DES.strip()],      
+                    }
+                    print(serialized_combined_data)
+                else:
+                    pass
